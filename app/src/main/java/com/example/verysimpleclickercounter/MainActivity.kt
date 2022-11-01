383e1a5a7e87bf6268370423d6496d29943da5c5
Whitecloudy <alex9395@naver.com>
2022-11-02 03:51:53 +0900

    Setting Menu Added

--- app/src/main/java/com/example/verysimpleclickercounter/MainActivity.kt
+++ app/src/main/java/com/example/verysimpleclickercounter/MainActivity.kt
@@ -1,16 +1,30 @@
 package com.example.verysimpleclickercounter
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.GestureDetector
 import android.view.KeyEvent
 import com.example.verysimpleclickercounter.databinding.ActivityMainBinding
+import android.view.MotionEvent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
+import kotlin.math.abs
 
 
-class MainActivity : Activity() {
-
+class MainActivity :
+    Activity(),
+    GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener
+{
     private lateinit var binding: ActivityMainBinding
-    var counter: Int = 0
+    private var counter: Int = 0
+    private lateinit var mDetector : GestureDetectorCompat
+    private val SWIPE_MIN_DISTANCE = 120
+    private val SWIPE_THRESHOLD_VELOCITY = 200
+
+    private val DEBUG_TAG = "Gestures"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +32,8 @@ class MainActivity : Activity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mDetector = GestureDetectorCompat(this, this)
+        mDetector.setOnDoubleTapListener(this)
         binding.numberView.text = counter.toString()
         binding.numberView.setOnLongClickListener{resetCounter()}
         binding.numberView.setOnClickListener{countOne()}
@@ -38,6 +54,78 @@ class MainActivity : Activity() {
         if (counter >= 1000)
             resetCounter()
         binding.numberView.text = counter.toString()
+        return true
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        return if (mDetector.onTouchEvent(event)) {
+            true
+        } else {
+            super.onTouchEvent(event)
+        }
+    }
+
+    override fun onDown(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onDown: $event")
+        return true
+    }
+
+    override fun onFling(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        try {
+            Log.d(DEBUG_TAG, "onFling: $(event1.y) $(event2.y) $velocityY")
+
+            if ((abs(event1.y - event2.y) > SWIPE_MIN_DISTANCE) and (abs(velocityY) > SWIPE_THRESHOLD_VELOCITY))
+            {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+            }
+        } catch (e: Exception) {
+
+        }
+
+        return true
+    }
+
+    override fun onLongPress(event: MotionEvent) {
+        Log.d(DEBUG_TAG, "onLongPress: $event")
+    }
+
+    override fun onScroll(
+        event1: MotionEvent,
+        event2: MotionEvent,
+        distanceX: Float,
+        distanceY: Float
+    ): Boolean {
+        Log.d(DEBUG_TAG, "onScroll: $event1 $event2")
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+        Log.d(DEBUG_TAG, "onShowPress: $event")
+    }
+
+    override fun onSingleTapUp(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onSingleTapUp: $event")
+        return true
+    }
+
+    override fun onDoubleTap(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onDoubleTap: $event")
+        return true
+    }
+
+    override fun onDoubleTapEvent(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onDoubleTapEvent: $event")
+        return true
+    }
+
+    override fun onSingleTapConfirmed(event: MotionEvent): Boolean {
+        Log.d(DEBUG_TAG, "onSingleTapConfirmed: $event")
         return true
     }
 
